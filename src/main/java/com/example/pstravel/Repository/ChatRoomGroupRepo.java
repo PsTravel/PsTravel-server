b66008d03a642541308f51e1800411a9c2a14d09
fitChan <84306157+fitChan@users.noreply.github.com>
2023-05-13 16:39:53 +0900

    jpa extends 변경

--- src/main/java/com/example/pstravel/Repository/ChatRoomGroupRepo.java
+++ src/main/java/com/example/pstravel/Repository/ChatRoomGroupRepo.java
@@ -4,6 +4,9 @@ import com.example.pstravel.Entity.ChatRoomGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ChatRoomGroupRepo extends JpaRepository<Long, ChatRoomGroup> {
+public interface ChatRoomGroupRepo extends JpaRepository<ChatRoomGroup,Long> {
+    Optional<ChatRoomGroup> findByUserId(Long id);
 }
--- src/main/java/com/example/pstravel/Repository/TravelPlanRepo.java
+++ src/main/java/com/example/pstravel/Repository/TravelPlanRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TravelPlanRepo extends JpaRepository<Long, TravelPlan> {
+public interface TravelPlanRepo extends JpaRepository<TravelPlan,Long> {
 }
